9f104141baaebfdd4a3f999b4f025691b548dc2b
Dillion Verma <dillionverma@hotmail.com>
2017-11-17 02:38:26 -0500

    setup watchlist, create list of tickers

--- js/crypto.js
+++ js/crypto.js
@@ -8,6 +8,22 @@ class Crypto {
     return $.get("https://api.coinmarketcap.com/v1/ticker/?limit=100");
   }
 
+  static watch(ticker) {
+    db.get("watchlist", function(list) {
+      list.push(ticker);
+      db.set("watchlist", list);
+    });
+  }
+
+  static unwatch(ticker) {
+    db.get("watchlist", function(list) {
+      list = list.filter(function(i) { 
+        return i !== ticker;
+      })
+      db.set("watchlist", list); 
+    });
+  }
+
 };
 
 
