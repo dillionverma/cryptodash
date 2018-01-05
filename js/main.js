26a2cd3a58ad18592772518bbc04a6fb964f6d34
Dillion Verma <dillionverma@hotmail.com>
2018-01-05 15:43:10 -0800

    finish watchlist functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ var searchBarInput = $("#search");
 var clearSearchBtn = $(".clearSearchBarField");
 var refreshBtn = $("#refresh");
 
+
+chrome.storage.sync.get("watchlist", function(vals) {
+  if (Object.keys(vals).length === 0) {
+    chrome.storage.sync.set({"watchlist": {}}, function(){});
+  }
+});
+
+chrome.storage.sync.get("watchlist-switch", function(vals) {
+  if (Object.keys(vals).length === 0) {
+    chrome.storage.sync.set({"watchlist-switch": false}, function(){});
+  }
+});
+
 $(document).ready(function(){
   $(".loader").fadeOut(600);
   updateTable();
@@ -26,7 +39,7 @@ $(document).ready(function(){
     updateTable();
   });
 
-  $('#ticker-table').on('click', 'tbody tr', function(){
+  $('#ticker-table').on('click', 'tbody tr td:nth-child(-n+6)', function(){
     var link = "https://coinmarketcap.com/currencies/" + $(this).data().id + "/";
     chrome.tabs.create({ url: link });
   });
@@ -35,32 +48,62 @@ $(document).ready(function(){
        
 function updateTable() {
   $.get("https://api.coinmarketcap.com/v1/ticker/?limit=500").done(function(response) {
-    var rows = '';
-    $.each(response, function (i, item) {
-      $tr = '<tr data-symbol="' + item.symbol + '" data-id="' + item.id + '">';
+    chrome.storage.sync.get("watchlist", function (d) {
+      var rows = '';
+      $.each(response, function (i, item) {
+        $tr = '<tr data-symbol="' + item.symbol + '" data-id="' + item.id + '">';
 
-      if (parseInt(item.percent_change_1h) > 0) {
-        $td = '<td class="green-text">';
-      } else {
-        $td = '<td class="red-text">';
-      }
-      if (parseInt(item.percent_change_24h) > 0) {
-        $td2 = '<td class="green-text">';
-      } else {
-        $td2 = '<td class="red-text">';
-      }
+        if (parseInt(item.percent_change_1h) > 0) {
+          $td = '<td class="green-text">';
+        } else {
+          $td = '<td class="red-text">';
+        }
+        if (parseInt(item.percent_change_24h) > 0) {
+          $td2 = '<td class="green-text">';
+        } else {
+          $td2 = '<td class="red-text">';
+        }
 
-      rows += $tr + 
-                  '<td>' + item.rank                                                          + '</td>' + 
-                  '<td>' + item.name                                                          + '</td>' + 
-                  '<td>' + item.symbol                                                        + '</td>' + 
-                  '<td>' + numeral(item.price_usd).format('$0,0.00[000]')                     + '</td>' + 
-                   $td   + numeral(item.percent_change_1h/100).format('0.00%')                + '</td>' + 
-                   $td2  + numeral(item.percent_change_24h/100).format('0.00%')               + '</td>' + 
-              '</tr>';
+        if (d.watchlist[item.name] == true) {
+          $td3 = '<td><input type="checkbox" class="watchlist" checked="checked" id="' + item.name + '" /><label for="' + item.name + '"></label></td>' 
+        } else {
+          $td3 = '<td><input type="checkbox" class="watchlist" id="' + item.name + '" /><label for="' + item.name + '"></label></td>' 
+        }
+
+        rows += $tr + 
+                    '<td>' + item.rank                                                          + '</td>' + 
+                    '<td>' + item.name                                                          + '</td>' + 
+                    '<td>' + item.symbol                                                        + '</td>' + 
+                    '<td>' + numeral(item.price_usd).format('$0,0.00[000]')                     + '</td>' + 
+                     $td   + numeral(item.percent_change_1h/100).format('0.00%')                + '</td>' + 
+                     $td2  + numeral(item.percent_change_24h/100).format('0.00%')               + '</td>' +
+                     $td3  +
+                '</tr>';
+      });
+      $('#ticker-table tbody').append(rows);
+
+      $('.watchlist').on('click', function() {
+        var name = $(this)[0].id;
+        console.log(name);
+        chrome.storage.sync.get("watchlist", function(d) {
+          var vals = d.watchlist;
+          if (vals[name] == null) {
+            vals[name] = true;
+            chrome.storage.sync.set({"watchlist": vals}, function() {
+              console.log("added");
+            });
+          } else if (vals[name] == true) {
+            delete vals[name];
+            chrome.storage.sync.set({"watchlist": vals}, function() {
+              console.log("removed");
+            });
+          }
+        });
+      });
+
+      tableSearch();
+      watchlist();
     });
-    $('#ticker-table tbody').append(rows);
-    tableSearch();
   });
 }
 
@@ -74,6 +117,28 @@ function tableSearch() {
     }).hide();
   });
 }
+
+
+function watchlist() {
+  var $checked = $('#ticker-table tbody tr td:nth-child(7)> .watchlist');
+  var $rows = $('#ticker-table tbody tr');
+  $('#watchlist-switch').on('click', function() {
+    chrome.storage.sync.get("watchlist-switch", function(s) {
+      chrome.storage.sync.set({"watchlist-switch": !s["watchlist-switch"]}, function() {
+        if (s["watchlist-switch"] == true) {
+          $rows.show();
+        } else {
+          $checked.each(function() {
+            if ($(this)[0].checked == false) {
+              $(this).parent().parent().hide();
+            }
+          });
+        } 
+      });
+    });
+  });
+}
+
 
 function resetInput() {
 	clearSearchBtn.hide();
